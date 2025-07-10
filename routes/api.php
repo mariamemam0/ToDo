08f7234c45c4211ecdf67e446a87da8045b337a5
mariam <mariamemampp00@gmail.com>
2025-07-10 15:49:54 +0300

    make route to controle projects that belong to user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,6 @@ Route::post('/login',[AuthController::class,'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    // You can add more routes that require authentication here
+    Route::apiResource('/projects',ProjectController::class);
 });
