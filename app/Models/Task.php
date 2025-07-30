8b91c5c58f93fa99cc8ab4547f3fbd080de27ba2
mariam <mariamemampp00@gmail.com>
2025-07-30 12:52:02 +0300

    model of the task

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+
+    protected $fillable=[
+        'title',
+        'is_completed',
+        'due_date',
+    ];
+    
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
